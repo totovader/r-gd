c70861acc8f04f402b40940bf22c6530790b2562
Totovader <totovader@gmail.com>
2009-09-30 23:46:04 -0500

    Added comment lines and a README

--- load.r
+++ load.r
@@ -1,7 +1,13 @@
-#gd load.r
+# gd load.r
+
+# grab gdp data (1929-2008) = Year, RealGDP
 gdp <- read.table("realgdp_import.csv", header=T, sep=",")
+
+# grab life expectancy data (1900-2004) = Year, LifeExpectancy
 le <- read.table("LifeExpectancy.csv", header=T, sep=",")
 
+# merge datasets on the Year, leave NA
 df <- merge(gdp, le, by="Year", all=T)
-df$Year <- as.Date(paste("01 01", as.character(df$Year)), "%m %d %Y")
 
+# convert Year/numeric to Year/Date for better plotting
+df$Year <- as.Date(paste("01 01", as.character(df$Year)), "%m %d %Y")
--- plot.r
+++ plot.r
@@ -1,15 +1,23 @@
-#gd plot.r
-library(ggplot2)
+# gd plot.r
 
+# TODO
+# output graph to png to generate OTF
+# create brew? script to output critical study to HTML/blog
+
+# get base GDP and LE for 1929 and create index
 base.gdp <- df$RealGDP[df$Year == as.Date("1929-01-01")]
 base.le <- df$LifeExpectancy[df$Year == as.Date("1929-01-01")]
 df$IndexGDP <- df$RealGDP / base.gdp
 df$IndexLE <- df$LifeExpectancy / base.le
 
+# plot whole dataset
 plot(df$Year, df$IndexGDP, type="l", ylim=range(df$IndexGDP, df$IndexLE, na.rm=T), xlab="Year", ylab="Index")
 lines(df$Year, df$IndexLE, col="green")
 
+# create cherry picked dataset for 1920 - 1940 to mimick Tapia Granados study
 cherry <- subset(df, df$Year >= as.Date("1920-01-01") & df$Year <= as.Date("1940-01-01"))
+
+# plot only cherry picked data and range
 plot(cherry$Year, cherry$IndexGDP, type="l", ylim=range(cherry$IndexGDP, cherry$IndexLE, na.rm=T), xlab="Year", ylab="Index")
 lines(cherry$Year, cherry$IndexLE, col="green")
 
